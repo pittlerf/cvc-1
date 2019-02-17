c8b28b9239187c69b1e0fafefa889101b754e8e9
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2019-02-17 20:22:56 +0100

    add mom_t zero_mom

--- const_arrays.cpp
+++ const_arrays.cpp
@@ -1,8 +1,11 @@
 #include "enums.hpp"
+#include "types.h"
+#include "const_arrays.hpp"
 
 namespace cvc {
 
 const char latDim_names[] = { 't', 'x', 'y', 'z' };
 const char shift_dir_names[] = { 'f', 'b' };
+const mom_t zero_mom{0, 0, 0};
 
 } // namespace(cvc)
---
+++ include/const_arrays.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace cvc {
+
+extern const char latDim_names[4];
+extern const char shift_dir_names[2];
+extern const mom_t zero_mom;
+
+} // namespace(cvc)
