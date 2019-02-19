3423211171e7b80ee524848d1f0477c5a7430955
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2019-02-19 13:17:55 +0100

    rework Logger again to now truly work as intended

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -22,33 +22,27 @@ private:
   int output_proc_id;
   int verbosity_threshold;
   NullBuffer nulbuf;
-  const std::ostream out_target;
-  std::ostream out;
+  std::ostream nul_stream;
+  std::ostream out_stream;
 
 public: 
   Logger() : 
-    out_target(std::cout.rdbuf()),
-    out(std::cout.rdbuf())
-  {
-    set_verbosity_threshold(0);
-    set_output_proc_id(0);
-  }
+    out_stream(std::cout.rdbuf()),
+    nul_stream(&nulbuf),
+    verbosity_threshold(0),
+    output_proc_id(0) {}
 
   Logger(const int proc_id_in, const int verbosity_threshold_in) :
-    out_target(std::cout.rdbuf()),
-    out(std::cout.rdbuf())
-  {
-    set_verbosity_threshold(verbosity_threshold_in);
-    set_output_proc_id(proc_id_in);
-  }
+    out_stream(std::cout.rdbuf()),
+    nul_stream(&nulbuf),
+    verbosity_threshold(verbosity_threshold_in),
+    output_proc_id(proc_id_in) {}
 
   Logger(const int proc_id_in, const int verbosity_threshold_in, std::ostream & out_in) :
-    out_target(out_in.rdbuf()),
-    out(out_in.rdbuf())
-  {
-    set_verbosity_threshold(verbosity_threshold_in);
-    set_output_proc_id(proc_id_in);
-  }
+    out_stream(out_in.rdbuf()),
+    nul_stream(&nulbuf),
+    verbosity_threshold(verbosity_threshold_in),
+    output_proc_id(proc_id_in) {}
 
   void set_verbosity_threshold(const int level)
   {
@@ -58,36 +52,34 @@ public:
   void set_output_proc_id(const int id)
   {
     output_proc_id = id;
-    // only the target process will actually direct its output
-    // to the target stream while all others will use the
-    // nulbuf
-    if( output_proc_id == g_proc_id ){
-      out.rdbuf(out_target.rdbuf());
-    } else {
-      out.rdbuf(&nulbuf);
-    }
   }
  
   template<typename T>
   std::ostream & operator<<(T val)
   {
-    if( verbosity_threshold <= g_verbose ){
-      return (out << val);
+    if( output_proc_id == g_proc_id && verbosity_threshold <= g_verbose ){
+      return (out_stream << val);
+    } else {
+      return (nul_stream << val);
     }
   }
 
   // support for io maniupulators and std::endl
   std::ostream & operator<<( std::ios_base & (*func)(std::ios_base &) )
   {
-    if( verbosity_threshold <= g_verbose ){
-      return (out << func);
+    if( output_proc_id == g_proc_id && verbosity_threshold <= g_verbose ){
+      return (out_stream << func);
+    } else {
+      return (nul_stream << func);
     }
   }
 
   std::ostream & operator<<( std::ostream& (*func)(std::ostream &) )
   {
-    if( verbosity_threshold <= g_verbose ){
-      return (out << func);
+    if( output_proc_id == g_proc_id && verbosity_threshold <= g_verbose ){
+      return (out_stream << func);
+    } else {
+      return (nul_stream << func);
     }
   }
 
