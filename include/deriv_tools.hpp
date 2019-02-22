7f48e0b4c7ce6ac7c7afe19bb26cf377ac9fca11
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2019-02-22 17:33:25 +0100

    silenced create_derivatives

--- include/deriv_tools.hpp
+++ include/deriv_tools.hpp
@@ -4,7 +4,6 @@
 #include "enums.hpp"
 
 #include <vector>
-#include <iostream>
 
 namespace cvc {
 
@@ -24,21 +23,16 @@ create_derivatives(
       }
     }
   } else {
-    std::cout << "[create_derivatives] Recursed from depth " << depth-1 << " to depth " << depth << 
-      ", max_depth:" << max_depth << std::endl;
     for( auto const & curr_deriv_chain : curr_derivs ){
-      std::cout << "[create_derivatives] curr_deriv_chain.size(): " << curr_deriv_chain.size() << std::endl;
       for( int dim = DIM_T; dim < DIM_NDIM; ++dim ){
         for( int dir = DIR_FWD; dir < DIR_NDIR; ++dir ){
           std::vector<deriv_t> deriv_chain = curr_deriv_chain;
           deriv_chain.push_back( deriv_t{dim, dir} );
-          std::cout << "[create_derivatives] deriv_chain.size(): " << deriv_chain.size() << std::endl;
           derivs.push_back( deriv_chain );
         }
       }
     }
   }
-  std::cout << "[create_derivatives] derivs.size(): " << derivs.size() << std::endl;
   
   if( depth+1 < max_depth ){
     return(create_derivatives(depth+1, max_depth, derivs));
