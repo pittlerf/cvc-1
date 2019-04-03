d01b51458ac1d1b95b5959fb590558cf7687c125
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2019-04-03 15:17:42 +0200

    properly document create_derivative_chains recursive function

--- include/deriv_tools.hpp
+++ include/deriv_tools.hpp
@@ -7,6 +7,68 @@
 
 namespace cvc {
 
+  /**
+   * @brief create a vector of vectors to represent chains of covariant derivatives
+   *
+   * A covariant derivative is applied in a given direction (forward or backward)
+   * and dimension (t,x,y,z <-> 0,1,2,3).
+   *
+   * A chain of such derivatives can also be applied in order, for example:
+   * 
+   *    D_0^f D_1^b D_2^f
+   *
+   * If taken as operator notation with the subscript denoting the space-time dimension
+   * and the superscript denoting the direction (f)orward, (b)ackward, this is read
+   * right to left.
+   *
+   * First, D_2^f is applied, then D_1^b and finally D_0^f.
+   *
+   * This function recursively creates a vector of vectors of pairs, each element 
+   * of type 'deriv_t'y  with elements .dim and .dir for the dimension and direction,
+   * respectively.
+   *
+   * To use it, one would create an empty std::vector< std::vector< cvc::deriv_t > >
+   * externally and pass it as the third argument to this function. One would
+   * specify 'depth = 0' to indicate that this is the first level and
+   * 'max_depth' specifies the number of derivative applications.
+   *
+   * The function will then create all combinations of (0,1,2,3) and (0,1) for
+   * the dimensions and directions, respectively, until the maximum depth is
+   * reached.
+   *
+   * For example (see tests/test_create_derivative_chains.cpp)
+   *
+   * std::vector< std::vector<deriv_t> > derivs;
+   * derivs = create_derivative_chains(0, 3, derivs);
+   *
+   * creates a vector of vectors with 512 elements of 3 elements each:
+   *
+   * (0,0) (0,0) (0,0) 
+   * (0,0) (0,0) (0,1) 
+   * (0,0) (0,0) (1,0) 
+   * (0,0) (0,0) (1,1) 
+   * (0,0) (0,0) (2,0) 
+   * (0,0) (0,0) (2,1)
+   * [...]
+   * (2,0) (3,1) (2,1) 
+   * (2,0) (3,1) (3,0) 
+   * (2,0) (3,1) (3,1) 
+   * (2,1) (0,0) (0,0) 
+   * (2,1) (0,0) (0,1) 
+   * (2,1) (0,0) (1,0)  
+   * [...]
+   * (3,1) (3,1) (1,1) 
+   * (3,1) (3,1) (2,0) 
+   * (3,1) (3,1) (2,1) 
+   * (3,1) (3,1) (3,0) 
+   * (3,1) (3,1) (3,1) 
+   *
+   * @param depth Current recursion level.
+   * @param max_depth Maximal recusrion level.
+   * @param curr_deriv_chains Current state of derivative chains vector.
+   *
+   * @return vector of vectors with derivative chains of the requested depth 
+   */
 static inline
 std::vector< std::vector<deriv_t> >
 create_derivative_chains(
--- tests/test_create_derivative_chains.cpp
+++ tests/test_create_derivative_chains.cpp
@@ -10,9 +10,9 @@ using namespace cvc;
 int main(int argc, char ** argv)
 {
   std::vector< std::vector<deriv_t> > derivs;
-  derivs = create_derivatives(0, 3, derivs);
+  derivs = create_derivative_chains(0, 3, derivs);
 
-  std::cout << "[test_create_derivatives] derivs.size(): " << derivs.size() << std::endl;
+  std::cout << "[test_create_derivative_chains] derivs.size(): " << derivs.size() << std::endl;
   for(auto const & deriv_chain : derivs){
     for( auto const & deriv : deriv_chain ){
       std::cout << "(" << deriv.dim << "," << deriv.dir << ") ";
