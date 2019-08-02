5070ac2a4070236fe7326b817f05ba49025e27e8
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2019-08-02 16:23:17 +0200

    cvc::exceptions: add constructors with char pointer arguments

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -6,6 +6,15 @@ namespace cvc {
 
 class runtime_error : public std::runtime_error {
   public:
+    runtime_error( const char * what_arg, const char * function_name ) :
+      runtime_error( std::string(what_arg), std::string(function_name) ) {}
+
+    runtime_error( const char * what_arg, const std::string& function_name ) :
+      runtime_error( std::string(what_arg), function_name ) {}
+
+    runtime_error( const std::string& what_arg, const char * function_name ) :
+      runtime_error( what_arg, std::string(function_name) ) {} 
+
     runtime_error( const std::string& what_arg, const std::string& function_name ) :
       std::runtime_error(what_arg)
     {
@@ -17,6 +26,15 @@ class runtime_error : public std::runtime_error {
 
 class invalid_argument : public std::invalid_argument {
   public:
+    invalid_argument( const char * what_arg, const char * function_name ) :
+      invalid_argument( std::string(what_arg), std::string(function_name) ) {}
+
+    invalid_argument( const char * what_arg, const std::string& function_name ) :
+      invalid_argument( std::string(what_arg), function_name ) {}
+
+    invalid_argument( const std::string& what_arg, const char * function_name ) :
+      invalid_argument( what_arg, std::string(function_name) ) {} 
+
     invalid_argument( const std::string& what_arg, const std::string& function_name ) :
       std::invalid_argument(what_arg)
     {
