c3d05898b45838dd9b9ab74a13c36e1ec69056dc
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2019-08-05 18:00:20 +0200

    add more comments about verbosity constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -8,13 +8,26 @@ namespace cvc {
   
   namespace verbosity {
     /**
+     * @brief verbosity level at which an extreme level of detail is passed to the log
+     */
+    constexpr int detailed_progress = 5;
+    /**
+     * @brief verbosity level at which all graph connection actions will be output in detail
+     */
+    constexpr int graph_connections = 4;
+    /**
      * @brief verbosity level at which input parsers will relay what they read
      */
     constexpr int input_relay = 3;
-    constexpr int graph_connections = 4;
+    /**
+     * @brief verbosity level at which [descend_and_resolve] reports in detail
+     */
     constexpr int resolve = 3;
+    /**
+     * @brief verbosity level at which detailed memory info is output before and after
+     *        contractions are done and written to disk
+     */
     constexpr int memory_info = 2;
     constexpr int basic_progress = 1;
-    constexpr int detailed_progress = 5;
   } //verbosity
 } //namespace(cvc)
