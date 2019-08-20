236d1147174793ed15e7354d38b8b997542540b7
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2019-08-20 19:09:58 +0200

    use autoreconf to set up LEMON source

--- travis-ci.sh
+++ travis-ci.sh
@@ -54,8 +54,7 @@ export LIMEDIR="$install_prefix"
 git clone https://github.com/etmc/lemon lemon
 pushd lemon
 lemon_srcdir="$(pwd)"
-aclocal
-autoconf
+autoreconf -vfi
 popd
 
 mkdir -p lemon_builddir
