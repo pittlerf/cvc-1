e8e7e38f59958dcbe158ea7adcf22d5ccd950f45
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2019-09-19 17:44:06 +0200

    add real support for non-full paths to h5::check_key_exists

--- include/h5utils.hpp
+++ include/h5utils.hpp
@@ -284,6 +284,8 @@ namespace h5 {
    * @param key Group / Dataset key of the form "/grp1/grp2/grp3/dataset1"
    * @param fail_path First link path of searched hierarchy which
    *                  could not be found. Note pass as reference.
+   * @param full_path Pass 'true' if the key refers to a complete
+   *                  path with a leading forward slash.
    *
    * @return true if path was found, false if it was not. Path of
    *         failure is returned vial fail_path argument.
@@ -293,46 +295,52 @@ namespace h5 {
                                       std::string & fail_path,
                                       bool full_path = true)
   {
-    char key_copy[H5UTILS_MAX_KEY_LENGTH];
-    if( strlen(key) < H5UTILS_MAX_KEY_LENGTH ){
-      strcpy( key_copy, key );
-    } else {
-      char message[500];
-      snprintf(message, 
-               500, 
-               "[cvc::h5::check_key_exists] length of key exceeds %d characters. %s line %d", 
-               H5UTILS_MAX_KEY_LENGTH, __FILE__, __LINE__);
-      EXIT_WITH_MSG(CVC_EXIT_H5_KEY_LENGTH, message);
-    }
-  
-    htri_t status;
-    std::string curr_path("");
-  
     if( full_path ){
+      char key_copy[H5UTILS_MAX_KEY_LENGTH];
+      if( strlen(key) < H5UTILS_MAX_KEY_LENGTH ){
+        strcpy( key_copy, key );
+      } else {
+        char message[500];
+        snprintf(message, 
+                 500, 
+                 "[cvc::h5::check_key_exists] length of key exceeds %d characters. %s line %d", 
+                 H5UTILS_MAX_KEY_LENGTH, __FILE__, __LINE__);
+        EXIT_WITH_MSG(CVC_EXIT_H5_KEY_LENGTH, message);
+      }
+  
+      htri_t status;
+      std::string curr_path("");
+  
       curr_path = "/";
       status = H5Lexists(loc_id, curr_path.c_str(), H5P_DEFAULT);
       if( status <= 0 ){
         fail_path = "/";
         return false;
       }
-    }
   
-    const char grp_sep[] = "/";
-    char * curr_grp_substr = strtok( key_copy, grp_sep );
-    while( curr_grp_substr != NULL ){
-      curr_path += std::string(curr_grp_substr);
-      status = H5Lexists(loc_id, curr_path.c_str(), H5P_DEFAULT);
-      if( status <= 0 ){
-        fail_path = curr_path;
-        return false;
+      const char grp_sep[] = "/";
+      char * curr_grp_substr = strtok( key_copy, grp_sep );
+      while( curr_grp_substr != NULL ){
+        curr_path += std::string(curr_grp_substr);
+        status = H5Lexists(loc_id, curr_path.c_str(), H5P_DEFAULT);
+        if( status <= 0 ){
+          fail_path = curr_path;
+          return false;
+        }
+        curr_grp_substr = strtok( NULL, grp_sep );
+        curr_path += std::string("/");
       }
-      curr_grp_substr = strtok( NULL, grp_sep );
-      curr_path += std::string("/");
-    }
-  
-    // traversal was successful
-    fail_path = "";
-    return true;
+      // traversal was successful
+      fail_path = "";
+      return true;
+    } else {
+      if( H5Lexists(loc_id, key, H5P_DEFAULT) <= 0 ){
+        return false;
+      } else {
+        fail_path = "";
+        return true;
+      }
+    } // if(full_path)
   }
 
 } // namespace(h5)
